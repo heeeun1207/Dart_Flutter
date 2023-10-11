e19f4952ae6cd8d5d80a74966c3d6d9e8f1dfef2
jungheeeun <jungheeeun272@gmail.com>
2023-10-11 12:38:11 +0900

    if - elseif - else

---
+++ dartprac/control/if.dart
@@ -0,0 +1,11 @@
+void main(){
+  int number=2;
+  
+  if( number % 3 == 0){
+    print('3의 배수입니다.');
+  }else if (number % 2 ==0){
+    print('2의 배수입니다.');
+  }else{
+    print('맞는 조건이 없습니다.');
+  }
+}
