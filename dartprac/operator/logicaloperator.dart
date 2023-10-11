9c06fb2986eea87ce0b40e9bca129fc50338bb82
jungheeeun <jungheeeun272@gmail.com>
2023-10-11 11:58:08 +0900

    논리연산자 && / ||

--- dartprac/operator/logicaloperator.dart
+++ dartprac/operator/logicaloperator.dart
@@ -1,3 +1,13 @@
 void main (){
-  
+  bool result = 12>10 && 1>0; //and 
+  print(result);
+
+  bool result2 = 12>10 && 0>1; 
+  print(result2);
+
+  bool result3 = 12>10 || 0>1; //or 
+  print(result3);
+
+  bool result4 = 12<10 || 0>1; 
+  print(result4);
 }
