e8e29818a7f85cb1ca0260d8c8687bb4ce5d228c
jungheeeun <jungheeeun272@gmail.com>
2023-10-12 16:42:00 +0900

    함수로 스트림 값 반환

---
+++ dartprac/Async/yield.dart
@@ -0,0 +1,25 @@
+import 'dart:async';
+
+//Stream을 반환하는 함수는 async*로 선언한다.
+Stream<String> calculate(int number) async*{
+  for(int i=0; i <5; i++){
+    //StreamController의 add() 처럼 yield 키워드를 이용해서 값 반환
+    yield 'i = $i';
+    await Future.delayed(Duration(seconds:1));
+  }
+}
+
+void playStream(){
+  //StreamController와 마찬가지로 listen 을 콜백함수로 입력 
+  calculate(1).listen((val){
+    print(val);
+  });
+}
+void main(){
+  playStream();
+}
+// i = 0
+// i = 1
+// i = 2
+// i = 3
+// i = 4
