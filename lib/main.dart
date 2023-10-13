cd910c9725ed191a7550f7917cbbfd62a4b513fe
jungheeeun <jungheeeun272@gmail.com>
2023-10-13 15:39:32 +0900

    기본 위젯 알아보기

--- lib/main.dart
+++ lib/main.dart
@@ -3,22 +3,20 @@
 // 플러터에서 제공하는 기본 위젯을 사용할 수 있다.
 import 'package:flutter/material.dart';
 
-void main (){
-  runApp(
-      MaterialApp( // 머터리얼 디자인 위젯
-        home:Scaffold( // Scaffold 위젯
-          body:SizedBox(
-            width: double.infinity,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              // 여러 위젯을 Column 위젯에 입력 가능
-              children: [
-                Text('Hello'),
-                Text('World'),
-              ],
-          ),
+void main () {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: //플러터 기본 위젯 알아보기 ,
         ),
       ),
-   ),
-);
+    )
+  }
 }
+
