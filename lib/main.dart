5dbfcde10c90a105ec4331711330be35ed6cbf22
jungheeeun <jungheeeun272@gmail.com>
2023-10-13 15:44:31 +0900

    텍스트관련 위젯-스타일적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,25 @@
-// 머터리얼 디자인과 관련된 기능을 불러오는 코드
-// material.dart 파일을 불러와야
-// 플러터에서 제공하는 기본 위젯을 사용할 수 있다.
 import 'package:flutter/material.dart';
 
-void main () {
+void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: //플러터 기본 위젯 알아보기 ,
+          child: Text(
+            '베토디',
+            style: TextStyle(
+              fontSize: 16.0,
+              fontWeight: FontWeight.w700,
+              color: Colors.red,
+            ),
+          ),
         ),
       ),
-    )
+    );
   }
 }
-
