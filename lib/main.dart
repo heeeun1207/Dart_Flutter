b26f62230230bc5e9f92b9bcb54ed112fdb305b8
jungheeeun <jungheeeun272@gmail.com>
2023-10-15 11:40:50 +0900

    FloatingActionButton 위젯

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(FloatingActionButtonExample());
 }
 
-class MyApp extends StatelessWidget {
+class FloatingActionButtonExample extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: GestureDetector(
-            // 1. 한 번 탭했을 때 실행할 함수
-            onTap: (){
-              // 출력 결과는 안드로이드 스튜디오 run 탭에서 확인 가능
-              print('on tap');
-            },
-            // 2. 두 번 탭했을 때 실행할 함수
-            onDoubleTap: (){
-              print('on double tap');
-            },
-            // 3. 길게 눌렀을 때 실행할 함수
-            onLongPress: (){
-              print('on long press');
-            },
-            // 제스쳐를 적용할 위젯
-            child:Container(
-              decoration : BoxDecoration(
-                color: Colors.red,
-              ),
-              width: 100.0,
-              height: 100.0,
-            ) ,
-          ),
+        floatingActionButton : FloatingActionButton(
+          // 클릭했을 때 실행할 함수
+          onPressed: (){},
+          child: Text('클릭'),
         ),
+              body : Container(),
       ),
     );
   }
