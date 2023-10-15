3cdb3950786db465918aed5d046beaf224fd1f13
jungheeeun <jungheeeun272@gmail.com>
2023-10-15 14:32:02 +0900

    디자인 관련 위젯들 알아보기 Container

---
+++ lib/style.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.red,
+              border: Border.all(
+                width: 16.0,
+                color: Colors.black,
+              ),
+              borderRadius: BorderRadius.circular(
+                16.0,
+              ),
+            ),
+            height: 200.0,
+              width: 100.0,
+          ),
+        ),
+      ),
+    );
+  }
+}
