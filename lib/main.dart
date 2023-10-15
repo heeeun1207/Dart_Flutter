638ec838355adfe9b80bb914eb3362ec6640669d
jungheeeun <jungheeeun272@gmail.com>
2023-10-15 11:30:11 +0900

    GestureDetector 위젯

--- lib/main.dart
+++ lib/main.dart
@@ -10,16 +10,32 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text(
-            '베토디',
-            style: TextStyle(
-              fontSize: 16.0,
-              fontWeight: FontWeight.w700,
-              color: Colors.red,
-            ),
+          child: GestureDetector(
+            // 1. 한 번 탭했을 때 실행할 함수
+            onTap: (){
+              // 출력 결과는 안드로이드 스튜디오 run 탭에서 확인 가능
+              print('on tap');
+            },
+            // 2. 두 번 탭했을 때 실행할 함수
+            onDoubleTap: (){
+              print('on double tap');
+            },
+            // 3. 길게 눌렀을 때 실행할 함수
+            onLongPress: (){
+              print('on long press');
+            },
+            // 제스쳐를 적용할 위젯
+            child:Container(
+              decoration : BoxDecoration(
+                color: Colors.red,
+              ),
+              width: 100.0,
+              height: 100.0,
+            ) ,
           ),
         ),
       ),
     );
   }
 }
+
