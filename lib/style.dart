22a136863a9fa10a95129244d58b39fca2f3905e
jungheeeun <jungheeeun272@gmail.com>
2023-10-16 12:55:29 +0900

    패딩위젯과 마진

--- lib/style.dart
+++ lib/style.dart
@@ -11,14 +11,24 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         body: Center(
           child: Container(
-            color: Colors.blue,
-            child: Padding(
-              // 상하 좌우로 모두 16픽셀만큼 패딩 적용
-              padding: EdgeInsets.all(16.0),
-              child: Container(
-                color: Colors.red,
-                width: 50.0,
-                height: 50.0,
+            color: Colors.black,
+
+            // 중간 파란 컨테이너
+            child: Container(
+              color: Colors.blue,
+
+              // 마진 적용 위치
+              margin: EdgeInsets.all(16.0),
+
+              // 패딩 적용
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                // 패딩이 적용된 빨간 컨테이너
+                child: Container(
+                  color: Colors.red,
+                  width: 50,
+                  height: 50,
+                ),
               ),
             ),
           ),
@@ -27,3 +37,6 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+// 패딩은 적용된 위젯이 차지하는 크기 내부에서 간격이 추가된다
+// 마진을 따로 위젯이 사용하지않고, 컨테이너 위젯에 추가 할 수있다 ( 많이 사용되진 않음)
