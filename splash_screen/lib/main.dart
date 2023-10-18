5dd00ec47d682c5d058473d5e892dafa2483b73c
jungheeeun <jungheeeun272@gmail.com>
2023-10-18 16:49:51 +0900

    Container와 BoxDecoration 위젯

--- splash_screen/lib/main.dart
+++ splash_screen/lib/main.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: SplashScreen(), // SplashScreen 위젯으로 첫 화면 지정
-  ));
+  runApp(SplashScreen()); // SplashScreen 위젯으로 첫 화면 지정
 }
 
-// SplashScreen 사용자 정의 위젯이 StatelessWidget을 상속함
 class SplashScreen extends StatelessWidget {
   @override
-  Widget build(BuildContext context) { // 위젯의 UI 구현
-    return Scaffold(
-      body: Center( // 중앙 정렬 위젯
-        // 글자에 화면을 보여주는 위젯
-        child: Text('Splash Screen'),
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            color: Colors.orange,
+          ),
+          child: Center(
+            child: Text('Splash Screen'),
+          ),
+        ),
       ),
     );
   }
