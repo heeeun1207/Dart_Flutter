78cc3dc0988bcc7929b941368800ffb49bc347ec
jungheeeun <jungheeeun272@gmail.com>
2023-10-20 14:16:45 +0900

    WebViewController 웹뷰 컨트롤러 설정

--- blog_web_app/lib/screen/home_screen.dart
+++ blog_web_app/lib/screen/home_screen.dart
@@ -3,8 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
-  // const 생성자
-  const HomeScreen({Key? key}) : super (key: key);
+  WebViewController? controller; // 컨트롤러 변수 생성
+  // const 생성자 -> 삭제
+  HomeScreen({Key? key}) : super (key: key);
 
   @override
   Widget build(BuildContext context){
@@ -17,7 +18,11 @@ class HomeScreen extends StatelessWidget {
         centerTitle: true,
       ),
       body: WebView( // WebView 추가
-        initialUrl: 'https://www.naver.com',
+        // 웹뷰 생성 함수
+        onWebViewCreated: (WebViewController controller){
+          this.controller =controller; // 위젯에 컨트롤러 저장
+        },
+        initialUrl: 'https://blog.codefactory.ai',
         javascriptMode: JavascriptMode.unrestricted,
       ),
     );
