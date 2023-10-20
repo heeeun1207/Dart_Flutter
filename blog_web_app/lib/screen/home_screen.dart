0b2c879f652014e8f59317474cac5636f0ab17ec
jungheeeun <jungheeeun272@gmail.com>
2023-10-20 12:17:58 +0900

    1. 앱바 구현

--- blog_web_app/lib/screen/home_screen.dart
+++ blog_web_app/lib/screen/home_screen.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-
   // const 생성자
   const HomeScreen({Key? key}) : super (key: key);
 
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      body: Text('Home Screen'),
+      appBar: AppBar( // 앱바 위젯 추가
+        backgroundColor: Colors.deepOrangeAccent,
+        // 앱 타이틀 설정
+        title: Text('Blog web app'),
+        // 가운데 정렬
+        centerTitle: true,
+      ),
+      body: Text('home screen'),
     );
   }
 }
