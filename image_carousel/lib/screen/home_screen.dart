e45b3edc831b0dd921ebcbcb66a089f29135101e
jungheeeun <jungheeeun272@gmail.com>
2023-10-24 00:00:31 +0900

    PageView 위젯 사용해서 이미지 스와이프

--- image_carousel/lib/screen/home_screen.dart
+++ image_carousel/lib/screen/home_screen.dart
@@ -6,7 +6,15 @@ class HomeScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      body: Text('Home Screen'),
+      body: PageView(
+        // PageView 여러 개의 위젯을 독단적인 페이지로 생성,
+        // 가로 또는 세로 스와이프로 페이지를 넘길 수 있는 위젯이다.
+        children: [1,2,3,4,5] // 샘플 리스트 생성
+          .map( // 위젯으로 매핑
+            (number) => Image.asset('asset/img/image_$number.jpeg'),
+           )
+            .toList(),
+      ),
     );
   }
 }
