3e62c9b4b15cf4ab6ee27320441aecbbd8cac458
jungheeeun <jungheeeun272@gmail.com>
2023-10-25 17:10:46 +0900

    pageController 로 PageView조작

--- image_carousel/lib/screen/home_screen.dart
+++ image_carousel/lib/screen/home_screen.dart
@@ -11,6 +11,8 @@ class HomeScreen extends StatefulWidget {
 }
 // _HomeScreenState 정의
 class _HomeScreenState extends State<HomeScreen>{
+  // PageController 생성
+  final PageController pageController = PageController();
 
   // initState() 함수 등록
   @override
@@ -20,7 +22,23 @@ class _HomeScreenState extends State<HomeScreen>{
     Timer.periodic(
       Duration(seconds: 3),
           (timer) {
-        print('실행!');
+
+        // 1.현재 페이지 가져오기
+        int? nextPage = pageController.page?.toInt();
+
+        if(nextPage == null){ // 2.페이지 값이 없을 때 예외 처리
+          return;
+        }
+        if(nextPage == 4){  // 3.첫 페이지와 마지막 페이지 분기 처리
+          nextPage = 0;
+          } else {
+          nextPage ++;
+        }
+        pageController.animateToPage( // 4.페이지 변경
+          nextPage,
+          duration: Duration(milliseconds: 500),
+          curve: Curves.ease,
+        );
       },
     );
 }
@@ -32,6 +50,7 @@ class _HomeScreenState extends State<HomeScreen>{
     return Scaffold(
       body: PageView(
         // PageView는 여러 개의 위젯을 독립적인 페이지로 표시하며, 사용자가 페이지를 가로 또는 세로로 스와이프하여 넘길 수 있는 위젯
+        controller: pageController,   // pageController 등록
         children: [1, 2, 3, 4, 5].map(
               (number) => Image.asset(
             'asset/img/image_$number.jpeg',
