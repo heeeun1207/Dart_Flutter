7980078f1832f3c8c855d4d657eec9bb7e6fb9dd
jungheeeun <jungheeeun272@gmail.com>
2023-10-25 12:33:00 +0900

    BoxFit.cover 이미지 핏 조절- 항상 전체화면

--- image_carousel/lib/screen/home_screen.dart
+++ image_carousel/lib/screen/home_screen.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget{
-  const HomeScreen ({Key? key}) : super(key: key);
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       body: PageView(
-        // PageView 여러 개의 위젯을 독단적인 페이지로 생성,
-        // 가로 또는 세로 스와이프로 페이지를 넘길 수 있는 위젯이다.
-        children: [1,2,3,4,5] // 샘플 리스트 생성
-          .map( // 위젯으로 매핑
-            (number) => Image.asset('asset/img/image_$number.jpeg'),
-           )
-            .toList(),
+        // PageView는 여러 개의 위젯을 독립적인 페이지로 표시하며, 사용자가 페이지를 가로 또는 세로로 스와이프하여 넘길 수 있는 위젯
+        children: [1, 2, 3, 4, 5].map(
+              (number) => Image.asset(
+            'asset/img/image_$number.jpeg',
+            fit: BoxFit.cover, // 이미지를 부모 위젯에 맞게 확대 또는 축소
+          ),
+        ).toList(),
       ),
     );
   }
