81a17fb075399c34e99ff16da6e02cb2fb8b4b12
jungheeeun <jungheeeun272@gmail.com>
2023-10-25 23:47:42 +0900

    _widgetname 언더스코어 위젯명 특징

--- u_and_i/lib/screen/home_screen.dart
+++ u_and_i/lib/screen/home_screen.dart
@@ -16,8 +16,8 @@ class HomeScreen extends StatelessWidget{
           // 반대축 최대 크기로 늘리기
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            _DDay(),
-            _CoupleImage()
+            _DDay(), // _첫글자가 언더스코어이면 다른 파일에서 접근할 수 없다.
+            _CoupleImage() // 파일을 불러오기 했을 때 불필요한 값들이 한 번에 불러와지는 걸 방지할 수있다.
           ],
         ),
       ),
