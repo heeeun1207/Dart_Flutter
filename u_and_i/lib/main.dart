53ce227648e8dc79d9866195b71b9df019666526
jungheeeun <jungheeeun272@gmail.com>
2023-10-26 14:27:06 +0900

    ThemeData 스타일 테마 적용 / 폰트 적용 왜 안될까?

--- u_and_i/lib/main.dart
+++ u_and_i/lib/main.dart
@@ -1,22 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:u_and_i/screen/home_screen.dart';
 
-void main(){
+void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData( // 테마를 지정할 수 있는 클래스
-        fontFamily: 'sunflower', // 기본 폰트
-        textTheme: TextTheme( // 글짜 테마를 적용할 수 있는 클래스
-          headline1: TextStyle(
+      theme: ThemeData(
+        fontFamily: 'sunflower',
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(
             color: Colors.white,
             fontSize: 80.0,
             fontWeight: FontWeight.w700,
             fontFamily: 'parisienne',
           ),
-        )
-
+          displayMedium:  TextStyle(
+            color: Colors.white,
+            fontSize: 40.0,
+            fontWeight: FontWeight.w700,
+          ),
+          bodyLarge:  TextStyle(
+            color: Colors.white,
+            fontSize: 30.0,
+          ),
+          bodyMedium:  TextStyle(
+            color: Colors.white,
+            fontSize: 20.0,
+          ),
+        ),
       ),
-      home: HomeScreen(),
+      home: const HomeScreen(),
     ),
   );
 }
