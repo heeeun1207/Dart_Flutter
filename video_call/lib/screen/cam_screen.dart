6220d905d49868b4ef2965bcc374ae6286b5f4d8
jungheeeun <jungheeeun272@gmail.com>
2023-11-27 15:33:02 +0900

    init() 비동기 -> 상태에 따라 처리하는 로직 만들기

--- video_call/lib/screen/cam_screen.dart
+++ video_call/lib/screen/cam_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:permission_handler/permission_handler.dart';
 class CamScreen extends StatefulWidget{
   const CamScreen ({Key? key}) : super(key: key);
 
@@ -8,16 +8,46 @@ class CamScreen extends StatefulWidget{
 }
 
 class _CamScreenState extends State<CamScreen>{
+  Future<bool> init() async { // 권한 관련 작업 모두 실행
+    final resp = await [Permission.camera, Permission.microphone].request();
+
+    final cameraPermission = resp[Permission.camera];
+    final micPermission = resp[Permission.microphone];
+
+    if(cameraPermission != PermissionStatus.granted ||
+    micPermission != PermissionStatus.granted){
+      throw '카메라 또는 마이크 권한이 없습니다.';
+    }
+    return true;
+  }
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('LIVE'),
+        title: const Text('LIVE'),
       ),
-      body: Center(
-      child: Text('Cam Screen'),
+      body: FutureBuilder(
+          future: init(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.hasError) { // 에러가 있을 때
+              return Center(
+                child: Text(
+                    snapshot.error.toString()
+                ),
+              );
+            }
+            if (!snapshot.hasData) { // 아직 데이터가 없을 때 ( = 로딩 중 )
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            return const Center( // 나머지 상황에 권한 있음 표시하기
+              child: Text('모든 권한이 있습니다!'),
+            );
+          }
       ),
     );
   }
-}
+  }
+
 
