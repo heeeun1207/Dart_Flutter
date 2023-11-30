c9b8a74858dd0834475f648575fa5d7655f48936
jungheeeun <jungheeeun272@gmail.com>
2023-11-30 18:29:23 +0900

    body : 구글 지도 확인

--- chool_check/lib/screen/home_screen.dart
+++ chool_check/lib/screen/home_screen.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeScreen extends StatelessWidget{
+  static final LatLng parkLatLng = LatLng( // 지도 초기화 위치
+    36.3567955, // 위도
+    127.3848277, // 경도
+  );
+
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context){
     return Scaffold(
       appBar: renderAppBar(),
-      body: Text('chool check app'),
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: parkLatLng,
+          zoom: 16, // 확대 정도 (높을수록 크게 보임)
+        ),
+      ),
     );
   }
 
