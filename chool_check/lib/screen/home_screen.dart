6cc0c4c5bea69e1453099d4f71b260b3a4d2b30c
jungheeeun <jungheeeun272@gmail.com>
2023-11-30 18:03:22 +0900

    AppBar : 중앙에 로고 만들기

--- chool_check/lib/screen/home_screen.dart
+++ chool_check/lib/screen/home_screen.dart
@@ -6,7 +6,24 @@ class HomeScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return Scaffold(
+      appBar: renderAppBar(),
       body: Text('chool check app'),
     );
   }
+
+  AppBar renderAppBar(){
+    // AppBar를 구현할 함수
+    return AppBar(
+      centerTitle: true,
+      title: Text(
+        '오늘도 출첵',
+        style: TextStyle(
+          color: Colors.blue,
+          fontWeight: FontWeight.w700,
+        ),
+      ),
+      backgroundColor: Colors.white,
+    );
+  }
 }
+
